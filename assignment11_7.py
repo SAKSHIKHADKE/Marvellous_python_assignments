c8a36513bd45dbbb7e3c421caf13b3ea01b95dde
SAKSHIKHADKE <sakshikhadke15@gmail.com>
2025-09-22 14:14:24 +0530

    Add files via upload

---
+++ assignment11_7.py
@@ -0,0 +1,29 @@
+"""def pattern(no) :
+    a = 1
+    while a <= no:
+        b = 1 
+        while b <= a:
+            print("*", end="")  
+            b = b + 1  
+        print()  
+        a = a + 1 """
+a = 1
+b = 1
+def pattern(no) :
+    global a
+    while a <= no: 
+        global b
+        while b <= a:
+            print("*", end=" ")  
+            b = b + 1  
+        print()  
+        a = a + 1 
+        pattern(no)
+        
+def main () :
+    no = int(input("enter the number : "))
+
+    pattern(no)
+
+if __name__ == "__main__" :
+    main()
